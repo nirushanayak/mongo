c61f0806675f337ec8d2dea8b5a7c6d7086bee56
nirushanayak <nirushanayak31@gmail.com>
2025-04-18 14:41:32 -0700

    basic_complied_file

--- src/mongo/db/pipeline/document_source_bidirectional_graph_lookup.cpp
+++ src/mongo/db/pipeline/document_source_bidirectional_graph_lookup.cpp
@@ -1,280 +1,158 @@
-#include "mongo/db/pipeline/document_source_bidirectional_graph_lookup.h"
+/**
+ *    Copyright (C) 2018-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ */
 
-#include <algorithm>
-#include <memory>
-#include <queue>
-#include <string>
-#include <vector>
+ #include "mongo/db/pipeline/document_source_bidirectional_graph_lookup.h"
+ #include "mongo/db/pipeline/document_source_graph_lookup.h"
+ #include "mongo/db/pipeline/expression_context.h"
+ #include "mongo/db/pipeline/lite_parsed_pipeline.h"
+ 
+ namespace mongo {
+ 
+ // Register the stage
+ REGISTER_DOCUMENT_SOURCE(bidirectionalGraphLookup,
+                          DocumentSourceBidirectionalGraphLookup::LiteParsed::parse,
+                          DocumentSourceBidirectionalGraphLookup::createFromBson,
+                          AllowedWithApiStrict::kAlways);
+ 
+ // Allocate ID for this stage
+ ALLOCATE_DOCUMENT_SOURCE_ID(bidirectionalGraphLookup, DocumentSourceBidirectionalGraphLookup::id)
 
-#include "mongo/db/exec/document_value/document.h"
-#include "mongo/db/exec/document_value/value.h"
-#include "mongo/db/pipeline/expression.h"
-#include "mongo/db/pipeline/expression_context.h"
-
-namespace mongo {
-
-// We need to manually handle document registration in a different way since
-// the modern MongoDB API is different from what we expected
-namespace {
-// Document source registration - using a "self-registration" approach
-struct RegisterBidirectionalGraphLookup {
-    RegisterBidirectionalGraphLookup() {
-        // Register our document source
-        static const auto kRegistry = Registry::get(getGlobalServiceContext());
-        DocumentSource::registerParser(
-            DocumentSourceBidirectionalGraphLookup::kStageName.toString(),
-            [](BSONElement elem, const boost::intrusive_ptr<ExpressionContext>& expCtx) {
-                return DocumentSourceBidirectionalGraphLookup::createFromBson(elem, expCtx);
-            });
-    }
-} registerBidirectionalGraphLookup;
-}
-
-std::unique_ptr<DocumentSourceBidirectionalGraphLookup::LiteParsed>
-DocumentSourceBidirectionalGraphLookup::LiteParsed::parse(const BSONElement& spec) {
+ std::unique_ptr<DocumentSourceBidirectionalGraphLookup::LiteParsed>
+DocumentSourceBidirectionalGraphLookup::LiteParsed::parse(
+    const NamespaceString& nss, const BSONElement& spec, const LiteParserOptions& options) {
+    
     uassert(ErrorCodes::FailedToParse,
-            str::stream() << "the " << DocumentSourceBidirectionalGraphLookup::kStageName
-                       << " stage specification must be an object",
-            spec.type() == BSONType::Object);
-
+            "$bidirectionalGraphLookup must be an object",
+            spec.type() == Object);
+            
     auto specObj = spec.Obj();
     auto fromElement = specObj["from"];
     uassert(ErrorCodes::FailedToParse,
-            str::stream() << "missing 'from' option to "
-                       << DocumentSourceBidirectionalGraphLookup::kStageName,
-            fromElement.type() == BSONType::String);
+            "missing 'from' option to $bidirectionalGraphLookup stage",
+            fromElement);
 
-    // Create a correct NamespaceString - the API seems to have changed
-    // Let's try using the constructor in different ways
-    NamespaceString fromNss;
-    try {
-        // Try with database + collection
-        fromNss = NamespaceString("admin." + fromElement.valueStringData());
-    } catch (const DBException& ex) {
-        uasserted(ErrorCodes::FailedToParse,
-                 str::stream() << "invalid foreign collection: " << fromElement.str());
-    }
-
-    return std::make_unique<DocumentSourceBidirectionalGraphLookup::LiteParsed>(
-        DocumentSourceBidirectionalGraphLookup::kStageName.toString(), std::move(fromNss));
-}
-
-PrivilegeVector DocumentSourceBidirectionalGraphLookup::LiteParsed::requiredPrivileges(
-    bool isMongos, bool bypassDocumentValidation) const {
-    PrivilegeVector privileges;
-    privileges.push_back({ResourcePattern::forExactNamespace(_foreignNss), ActionType::find});
-    return privileges;
-}
-
-stdx::unordered_set<NamespaceString>
-DocumentSourceBidirectionalGraphLookup::LiteParsed::getInvolvedNamespaces() const {
-    return {_foreignNss};
-}
-
-boost::intrusive_ptr<DocumentSource> DocumentSourceBidirectionalGraphLookup::createFromBson(
-    BSONElement elem, const boost::intrusive_ptr<ExpressionContext>& pExpCtx) {
-    
     uassert(ErrorCodes::FailedToParse,
-            str::stream() << "the " << kStageName << " stage specification must be an object",
-            elem.type() == BSONType::Object);
-
-    auto specObj = elem.Obj();
-
-    // Parse required fields
-    std::string fromFieldName = parseRequiredStringField(specObj, "from");
-    std::string asFieldName = parseRequiredStringField(specObj, "as");
-    std::string connectToFieldName = parseRequiredStringField(specObj, "connectToField");
-    std::string connectFromFieldName = parseRequiredStringField(specObj, "connectFromField");
-
-    // Parse optional fields
-    boost::optional<BSONObj> startWith;
-    if (specObj.hasField("startWith")) {
-        startWith = specObj["startWith"].Obj().getOwned();
-    }
-
-    boost::optional<BSONObj> endWith;
-    if (specObj.hasField("endWith")) {
-        endWith = specObj["endWith"].Obj().getOwned();
-    }
-
-    boost::optional<std::string> depthField;
-    if (specObj.hasField("depthField")) {
-        depthField = parseRequiredStringField(specObj, "depthField");
-    }
-
-    boost::optional<long long> maxDepth;
-    if (specObj.hasField("maxDepth")) {
-        uassert(ErrorCodes::FailedToParse,
-                str::stream() << "maxDepth must be a number, but got " 
-                           << typeName(specObj["maxDepth"].type()),
-                specObj["maxDepth"].isNumber());
-        maxDepth = specObj["maxDepth"].safeNumberLong();
-    }
-
-    // Create namespace string - using a simpler approach since pExpCtx->ns is not available
-    NamespaceString fromNs("admin." + fromFieldName);
-
-    // Create and return the pipeline stage
-    return boost::intrusive_ptr<DocumentSource>(new DocumentSourceBidirectionalGraphLookup(
-        pExpCtx,
-        std::move(fromNs),
-        asFieldName,
-        connectToFieldName,
-        connectFromFieldName,
-        startWith,
-        endWith,
-        depthField,
-        maxDepth));
-}
-
-DocumentSourceBidirectionalGraphLookup::DocumentSourceBidirectionalGraphLookup(
-    const boost::intrusive_ptr<ExpressionContext>& pExpCtx,
-    NamespaceString fromNs,
-    std::string asField,
-    std::string connectToField,
-    std::string connectFromField,
-    boost::optional<BSONObj> startWith,
-    boost::optional<BSONObj> endWith,
-    boost::optional<std::string> depthField,
-    boost::optional<long long> maxDepth)
-    : DocumentSource(kStageName, pExpCtx),
-      _fromNs(std::move(fromNs)),
-      _asField(std::move(asField)),
-      _connectToField(std::move(connectToField)),
-      _connectFromField(std::move(connectFromField)),
-      _startWith(std::move(startWith)),
-      _endWith(std::move(endWith)),
-      _depthField(std::move(depthField)),
-      _maxDepth(std::move(maxDepth)) {}
-
-StageConstraints DocumentSourceBidirectionalGraphLookup::constraints(Pipeline::SplitState pipeState) const {
-    return StageConstraints(
-        StreamType::kStreaming,
-        PositionRequirement::kNone,
-        HostTypeRequirement::kNone,
-        DiskUseRequirement::kNoDiskUse,
-        FacetRequirement::kAllowed,
-        TransactionRequirement::kAllowed,
-        LookupRequirement::kAllowed,
-        UnionRequirement::kAllowed);
-}
-
-DocumentSource::GetNextResult DocumentSourceBidirectionalGraphLookup::doGetNext() {
-    if (_sourceExhausted) {
-        return GetNextResult::makeEOF();
-    }
-
-    if (_results.empty()) {
-        // Get the next input document
-        auto nextInput = pSource->getNext();
-        if (nextInput.isEOF()) {
-            _sourceExhausted = true;
-            return GetNextResult::makeEOF();
-        }
-
-        // Process the input document through bidirectional algorithm
-        return performBidirectionalSearch(nextInput.releaseDocument());
-    }
-
-    // Return the next queued result
-    auto nextResult = std::move(_results.front());
-    _results.pop();
-    return DocumentSource::GetNextResult(std::move(nextResult));
-}
-
-DocumentSource::GetNextResult DocumentSourceBidirectionalGraphLookup::performBidirectionalSearch(
-    const Document& inputDoc) {
+            "$bidirectionalGraphLookup 'from' field must be a string",
+            fromElement.type() == String);
     
-    // This is a placeholder implementation
-    // In a real implementation, you would implement the bidirectional search algorithm
+    // Direct namespace construction - try a simpler approach
+    NamespaceString fromNs;
     
-    // For now, just return the input document with an empty array field
-    MutableDocument result(inputDoc);
-    result.setField(_asField, Value(std::vector<Value>{}));
-    return DocumentSource::GetNextResult(result.freeze());
-}
+    // Parse the from namespace directly
+    if (fromElement.valueStringData().find('.') == std::string::npos) {
+        // Collection in the current database
+        // fromNs = NamespaceString(nss.db(), fromElement.valueStringData());
+        fromNs = NamespaceStringUtil::deserialize(nss.dbName(), fromElement.valueStringData());
 
-std::vector<Value> DocumentSourceBidirectionalGraphLookup::getStartIds(const Document& inputDoc) {
-    // Placeholder implementation
-    return std::vector<Value>{};
-}
+    } else {
+        // Fully qualified namespace
+        // fromNs = NamespaceString(fromElement.valueStringData());
+        fromNs = NamespaceStringUtil::deserialize(nss.dbName(), fromElement.valueStringData());
 
-std::vector<Value> DocumentSourceBidirectionalGraphLookup::getTargetIds(const Document& inputDoc) {
-    // Placeholder implementation
-    return std::vector<Value>{};
-}
-
-std::vector<Value> DocumentSourceBidirectionalGraphLookup::expandForward(const Value& nodeId) {
-    // Placeholder implementation
-    return std::vector<Value>{};
-}
-
-std::vector<Value> DocumentSourceBidirectionalGraphLookup::expandBackward(const Value& nodeId) {
-    // Placeholder implementation
-    return std::vector<Value>{};
-}
-
-boost::optional<Document> DocumentSourceBidirectionalGraphLookup::reconstructPath(Value meetingId) {
-    // Placeholder implementation
-    return boost::none;
-}
-
-std::vector<Document> DocumentSourceBidirectionalGraphLookup::lookupDocumentsByIds(
-    const std::vector<Value>& ids) {
-    // Placeholder implementation
-    return std::vector<Document>{};
-}
-
-std::vector<Document> DocumentSourceBidirectionalGraphLookup::findDocumentsThatReferenceId(
-    const Value& id) {
-    // Placeholder implementation
-    return std::vector<Document>{};
-}
-
-Value DocumentSourceBidirectionalGraphLookup::serialize(const SerializationOptions& opts) const {
-    MutableDocument result;
-    
-    Document config(
-        {{"from", Value(_fromNs.coll())},
-         {"as", Value(_asField)},
-         {"connectToField", Value(_connectToField)},
-         {"connectFromField", Value(_connectFromField)}});
-    
-    MutableDocument configDoc(config);
-    
-    if (_startWith) {
-        configDoc.setField("startWith", Value(_startWith.get()));
     }
     
-    if (_endWith) {
-        configDoc.setField("endWith", Value(_endWith.get()));
-    }
+    // Validate the namespace
+    uassert(ErrorCodes::InvalidNamespace,
+            str::stream() << "invalid $bidirectionalGraphLookup namespace: "
+                        << fromNs.toStringForErrorMsg(),
+            fromNs.isValid());
     
-    if (_depthField) {
-        configDoc.setField("depthField", Value(*_depthField));
-    }
-    
-    if (_maxDepth) {
-        configDoc.setField("maxDepth", Value(*_maxDepth));
-    }
-    
-    result.setField(kStageName.toString(), Value(configDoc.freeze()));
-    return Value(result.freeze());
+    return std::make_unique<LiteParsed>(spec.fieldName(), std::move(fromNs));
 }
-
-// Helper function to parse required string fields from BSON
-std::string parseRequiredStringField(const BSONObj& obj, StringData fieldName) {
-    auto elem = obj[fieldName];
-    uassert(ErrorCodes::FailedToParse,
-            str::stream() << "missing required field '" << fieldName << "'",
-            !elem.eoo());
-    
-    uassert(ErrorCodes::FailedToParse,
-            str::stream() << "'" << fieldName << "' must be a string, not " 
-                       << typeName(elem.type()),
-            elem.type() == BSONType::String);
-            
-    return elem.String();
-}
-
-} // namespace mongo
+ 
+ PrivilegeVector DocumentSourceBidirectionalGraphLookup::LiteParsed::requiredPrivileges(
+     bool isMongos, bool bypassDocumentValidation) const {
+     return {Privilege(ResourcePattern::forExactNamespace(_foreignNss), ActionType::find)};
+ }
+ 
+ boost::intrusive_ptr<DocumentSource>
+ DocumentSourceBidirectionalGraphLookup::createFromBson(
+     BSONElement elem, const boost::intrusive_ptr<ExpressionContext>& pExpCtx) {
+     
+     // For now, just create the basic stage
+     return make_intrusive<DocumentSourceBidirectionalGraphLookup>(pExpCtx);
+ }
+ 
+ const char* DocumentSourceBidirectionalGraphLookup::getSourceName() const {
+     return kStageName.rawData();
+ }
+ 
+ DocumentSourceBidirectionalGraphLookup::Id DocumentSourceBidirectionalGraphLookup::getId() const {
+     return id;
+ }
+ 
+ DocumentSource::GetNextResult DocumentSourceBidirectionalGraphLookup::doGetNext() {
+     // For now, just pass through documents
+     return pSource->getNext();
+ }
+ 
+ void DocumentSourceBidirectionalGraphLookup::doDispose() {
+     // Nothing to dispose yet
+ }
+ 
+ boost::intrusive_ptr<DocumentSource>
+ DocumentSourceBidirectionalGraphLookup::clone(
+     const boost::intrusive_ptr<ExpressionContext>& pExpCtx) const {
+     
+     return make_intrusive<DocumentSourceBidirectionalGraphLookup>(pExpCtx);
+ }
+ 
+ Value DocumentSourceBidirectionalGraphLookup::serialize(
+     const SerializationOptions& opts) const {
+     
+     MutableDocument container;
+     container[getSourceName()] = Value(Document{});
+     return container.freezeToValue();
+ }
+ 
+ void DocumentSourceBidirectionalGraphLookup::serializeToArray(
+     std::vector<Value>& array, const SerializationOptions& opts) const {
+     
+     array.push_back(serialize(opts));
+ }
+ 
+ DocumentSource::GetModPathsReturn
+ DocumentSourceBidirectionalGraphLookup::getModifiedPaths() const {
+     // No paths modified yet
+     return {GetModPathsReturn::Type::kFiniteSet, {}, {}};
+ }
+ 
+ StageConstraints DocumentSourceBidirectionalGraphLookup::constraints(
+     Pipeline::SplitState pipeState) const {
+     
+     return StageConstraints(
+         StreamType::kStreaming,
+         PositionRequirement::kNone,
+         HostTypeRequirement::kNone,
+         DiskUseRequirement::kNoDiskUse,
+         FacetRequirement::kAllowed,
+         TransactionRequirement::kAllowed,
+         LookupRequirement::kAllowed,
+         UnionRequirement::kAllowed);
+ }
+ 
+ boost::optional<DocumentSource::DistributedPlanLogic>
+ DocumentSourceBidirectionalGraphLookup::distributedPlanLogic() {
+     return boost::none;
+ }
+ 
+ boost::optional<ShardId>
+ DocumentSourceBidirectionalGraphLookup::computeMergeShardId() const {
+     return boost::none;
+ }
+ 
+ void DocumentSourceBidirectionalGraphLookup::detachFromOperationContext() {
+     // No-op for now
+ }
+ 
+ void DocumentSourceBidirectionalGraphLookup::reattachToOperationContext(
+     OperationContext* opCtx) {
+     // No-op for now
+ }
+ 
+ }  // namespace mongo
--- src/mongo/db/pipeline/document_source_bidirectional_graph_lookup.h
+++ src/mongo/db/pipeline/document_source_bidirectional_graph_lookup.h
@@ -1,157 +1,72 @@
-#pragma once
-
-#include <cstddef>
-#include <memory>
-#include <queue>
-#include <string>
-#include <unordered_map>
-#include <vector>
-
-#include <boost/optional/optional.hpp>
-#include <boost/smart_ptr/intrusive_ptr.hpp>
-
-#include "mongo/base/error_codes.h"
-#include "mongo/base/string_data.h"
-#include "mongo/bson/bsonelement.h"
-#include "mongo/bson/bsonobj.h"
-#include "mongo/db/auth/action_type.h"
-#include "mongo/db/auth/privilege.h"
-#include "mongo/db/auth/resource_pattern.h"
-#include "mongo/db/exec/document_value/document.h"
-#include "mongo/db/exec/document_value/value.h"
-#include "mongo/db/exec/document_value/value_comparator.h"
-#include "mongo/db/namespace_string.h"
-#include "mongo/db/pipeline/document_source.h"
-#include "mongo/db/pipeline/expression_context.h"
-#include "mongo/db/pipeline/lite_parsed_document_source.h"
-#include "mongo/db/pipeline/stage_constraints.h"
-#include "mongo/stdx/unordered_set.h"
-
-namespace mongo {
-
 /**
- * This stage implements bidirectional graph lookup - an optimized path finding algorithm
- * that searches from both start and end nodes simultaneously to find paths more efficiently
- * than standard BFS used in $graphLookup.
+ *    Copyright (C) 2018-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
  */
-class DocumentSourceBidirectionalGraphLookup : public DocumentSource {
-public:
-    static constexpr StringData kStageName = "$bidirectionalGraphLookup"_sd;
 
-    class LiteParsed final : public LiteParsedDocumentSource {
-    public:
-        static std::unique_ptr<LiteParsed> parse(const BSONElement& spec);
+ #pragma once
 
-        LiteParsed(std::string parseTimeName, NamespaceString foreignNss)
-            : LiteParsedDocumentSource(std::move(parseTimeName)), _foreignNss(std::move(foreignNss)) {}
-
-        PrivilegeVector requiredPrivileges(bool isMongos, 
-                                          bool bypassDocumentValidation) const override;
-
-        stdx::unordered_set<NamespaceString> getInvolvedNamespaces() const override;
-
-    private:
-        const NamespaceString _foreignNss;
-    };
-
-    static boost::intrusive_ptr<DocumentSource> createFromBson(
-        BSONElement elem, 
-        const boost::intrusive_ptr<ExpressionContext>& pExpCtx);
-
-    // Implement required DocumentSource pure virtual methods
-    const char* getSourceName() const override { return kStageName.rawData(); }
-    
-    // Implement the pipeline stage logic pure virtual methods
-    StageConstraints constraints(Pipeline::SplitState pipeState) const override;
-    Value serialize(const SerializationOptions& opts = SerializationOptions{}) const override;
-    
-    // Variable reference tracking
-    void addVariableRefs(std::set<Variables::Id>* refs) const override {
-        // No variables used in this stage
-    }
-    
-    // Distributed plan logic
-    boost::optional<DistributedPlanLogic> distributedPlanLogic() override {
-        return boost::none;  // Not distributable
-    }
-
-protected:
-    // This is the core processing method
-    GetNextResult doGetNext() override;
-
-private:
-    // Custom hasher and equality for Value
-    struct ValueHasher {
-        size_t operator()(const Value& v) const {
-            return std::hash<std::string>{}(v.toString());
-        }
-    };
-
-    struct ValueEqualTo {
-        bool operator()(const Value& lhs, const Value& rhs) const {
-            return ValueComparator().compare(lhs, rhs) == 0;
-        }
-    };
-
-    // Internal data structures for the bidirectional search
-    struct SearchNode {
-        Value id;
-        size_t depth;
-        boost::optional<Value> parentId;
-    };
-
-    // Constructor - private, use createFromBson instead
-    DocumentSourceBidirectionalGraphLookup(
-        const boost::intrusive_ptr<ExpressionContext>& pExpCtx,
-        NamespaceString fromNs,
-        std::string asField,
-        std::string connectToField,
-        std::string connectFromField,
-        boost::optional<BSONObj> startWith,
-        boost::optional<BSONObj> endWith,
-        boost::optional<std::string> depthField,
-        boost::optional<long long> maxDepth);
-
-    // Core algorithm implementation
-    GetNextResult performBidirectionalSearch(const Document& inputDoc);
-    
-    // Helper methods
-    std::vector<Value> getStartIds(const Document& inputDoc);
-    std::vector<Value> getTargetIds(const Document& inputDoc);
-    std::vector<Value> expandForward(const Value& nodeId);
-    std::vector<Value> expandBackward(const Value& nodeId);
-    boost::optional<Document> reconstructPath(Value meetingId);
-
-    // Database access methods
-    std::vector<Document> lookupDocumentsByIds(const std::vector<Value>& ids);
-    std::vector<Document> findDocumentsThatReferenceId(const Value& id);
-    
-    // Configuration
-    NamespaceString _fromNs;
-    std::string _asField;
-    std::string _connectToField;
-    std::string _connectFromField;
-    boost::optional<BSONObj> _startWith;
-    boost::optional<BSONObj> _endWith;
-    boost::optional<std::string> _depthField;
-    boost::optional<long long> _maxDepth;
-    
-    // Algorithm state
-    bool _sourceExhausted = false;
-    std::queue<Document> _results;
-    
-    // Bidirectional search state with custom hashers
-    std::unordered_map<Value, SearchNode, ValueHasher, ValueEqualTo> _forwardVisited;
-    std::unordered_map<Value, SearchNode, ValueHasher, ValueEqualTo> _backwardVisited;
-    std::queue<Value> _forwardQueue;
-    std::queue<Value> _backwardQueue;
-    std::vector<Document> _allNodes;
-    
-    // Cache for lookups
-    std::unordered_map<Value, Document, ValueHasher, ValueEqualTo> _documentCache;
-};
-
-// Helper function declaration
-std::string parseRequiredStringField(const BSONObj& obj, StringData fieldName);
-
-} // namespace mongo
+ #include "mongo/db/pipeline/document_source.h"
+ #include "mongo/db/pipeline/document_source_graph_lookup.h"
+ 
+ namespace mongo {
+ 
+ /**
+  * $bidirectionalGraphLookup is a stage that performs a graph lookup using a bidirectional search.
+  */
+ class DocumentSourceBidirectionalGraphLookup final : public DocumentSource {
+ public:
+     static constexpr StringData kStageName = "$bidirectionalGraphLookup"_sd;
+     static const Id& id;
+     
+     // Required implementations for DocumentSource
+     const char* getSourceName() const final;
+     Id getId() const final;
+     
+     // Create a simple implementation for addVariableRefs (required to avoid 'abstract class' error)
+     void addVariableRefs(std::set<Variables::Id>* refs) const final {}
+     
+     boost::intrusive_ptr<DocumentSource> clone(
+         const boost::intrusive_ptr<ExpressionContext>& pExpCtx) const final;
+     Value serialize(const SerializationOptions& opts = SerializationOptions{}) const final;
+     void serializeToArray(std::vector<Value>& array, 
+                          const SerializationOptions& opts = SerializationOptions{}) const final;
+     GetModPathsReturn getModifiedPaths() const final;
+     StageConstraints constraints(Pipeline::SplitState pipeState) const final;
+     boost::optional<DistributedPlanLogic> distributedPlanLogic() final;
+     
+     // Static factory method for creating from BSON
+     static boost::intrusive_ptr<DocumentSource> createFromBson(
+         BSONElement elem, const boost::intrusive_ptr<ExpressionContext>& pExpCtx);
+         
+     // Constructors
+     explicit DocumentSourceBidirectionalGraphLookup(const boost::intrusive_ptr<ExpressionContext>& pExpCtx)
+         : DocumentSource(kStageName, pExpCtx) {}
+         
+     // LiteParsed class for early parsing
+     class LiteParsed final : public LiteParsedDocumentSourceForeignCollection {
+     public:
+         static std::unique_ptr<LiteParsed> parse(const NamespaceString& nss,
+                                                 const BSONElement& spec,
+                                                 const LiteParserOptions& options);
+ 
+         LiteParsed(std::string parseTimeName, NamespaceString foreignNss)
+             : LiteParsedDocumentSourceForeignCollection(std::move(parseTimeName),
+                                                       std::move(foreignNss)) {}
+ 
+         PrivilegeVector requiredPrivileges(bool isMongos, bool bypassDocumentValidation) const override;
+     };
+     
+ protected:
+     GetNextResult doGetNext() final;
+     void doDispose() final;
+     boost::optional<ShardId> computeMergeShardId() const final;
+     
+ private:
+     void detachFromOperationContext() final;
+     void reattachToOperationContext(OperationContext* opCtx) final;
+ };
+ 
+ }  // namespace mongo
